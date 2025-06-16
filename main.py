5d1394f7b91ed32687bf1f6865c9837926f0bb18
Vinicios Falqueiro <vinicios.falqueiro@gmail.com>
2025-06-16 20:41:21 -0300

    alterando api

--- main.py
+++
@@ -1,9 +0,0 @@
-from openai import OpenAI
-client = OpenAI()
-
-response = client.responses.create(
-    model="gpt-4.1",
-    input="Write a one-sentence bedtime story about a unicorn."
-)
-
-print(response.output_text)
